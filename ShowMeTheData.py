921b9d268451663fcb9dbfc47ce91b87b5de0b8b
AineNicD <59982405+AineNicD@users.noreply.github.com>
2020-04-25 04:53:36 +0100

    Added a file called "ShowMeTheData".
    I was researching different ways I can add the data into my code and came across this on the website I referenced. It prints a detailed description of the Iris Data set.

---
+++ ShowMeTheData.py
@@ -0,0 +1,12 @@
+## Import data
+from sklearn.datasets import load_iris 
+iris_dataset = load_iris()
+
+print(iris_dataset['DESCR']) 
+
+
+#I found this code at:
+#https://rpubs.com/nandong/imlp-ch1-iris
+
+
+
