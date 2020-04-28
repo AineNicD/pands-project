91040e6497fea148c2f46978f79cb5d49a4cf41c
AineNicD <59982405+AineNicD@users.noreply.github.com>
2020-04-28 11:55:42 +0100

    added a scatter plot of each pair of variables
    For this code I got great help from the Seaborn Library, reference in the code and the Read me, seaborn has many features and simple lines of code for producing plots. It always amazes me when I research for a long time and find that what I want to do can be done with one or two lines of codes. Python is incredible that way.

---
+++ scatterVar.py
@@ -0,0 +1,24 @@
+import numpy as np
+import pandas as pd
+from pandas.plotting import scatter_matrix
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Reading Iris Dataset in Pandas Dataframe
+data = pd.read_csv("IrisDataSet.csv")
+names = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'species']
+
+
+# https://www.kaggle.com/farheen28/iris-dataset-analysis-using-knn
+#https://seaborn.pydata.org/generated/seaborn.PairGrid.html
+#seaborn, there are so many great codes for analysis in the Seaborn library
+#The links above helped me deveop this for the scatterplots of each pair
+#after a few failed attemps and playing around with it,
+#it simplified to two lines of code for both.
+
+sns.FacetGrid(data, hue="species", height=6).map(plt.scatter, "sepal_length", "sepal_width").add_legend() 
+sns.FacetGrid(data, hue="species", height=6).map(plt.scatter, "petal_length", "petal_width").add_legend() 
+
+plt.show()
+
+#https://www.kaggle.com/anthonyhills/classifying-species-of-iris-flowers
